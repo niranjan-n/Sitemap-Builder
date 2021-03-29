293b815ef7a310fc4d191f78c041c2d7131e36e6
niranjan-n <hegdeniranjan97@gmail.com>
2021-03-29 22:39:37 +0530

    filtering links

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -14,7 +15,14 @@ func main() {
 	urlFlag := flag.String("url", "https://golang.org/", "The url that you want to build a sitemap for")
 	flag.Parse()
 	fmt.Println(*urlFlag)
-	resp, err := http.Get(*urlFlag)
+	hrefs := getBaseURL(*urlFlag)
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+
+}
+func getBaseURL(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
@@ -27,18 +35,38 @@ func main() {
 		Host:   reqURL.Host,
 	}
 	base := baseURL.String()
-	links, _ := link.Parse(resp.Body)
-	var hrefs []string
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
+			ret = append(ret, l.Href)
 		}
 	}
-	for _, href := range hrefs {
-		fmt.Println(href)
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+
+	return func(link string) bool {
+
+		return strings.HasPrefix(link, pfx)
 	}
 
 }
